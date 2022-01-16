f5b049a1a57f0fc1c798c03eb2681c2dbe4546a9
Gunn Hwang <gunhwang99@gmail.com>
2022-01-15 20:34:20 -0800

    Bug fix

--- src/components/App.js
+++ src/components/App.js
@@ -7,23 +7,20 @@ import PortfolioPost from "./Portfolio/PortfolioPost";
 import Blog from "./Blog/Blog";
 import Post from "./Blog/Post";
 import ContactPage from "./ContactPage/ContactPage";
-import { gsap, TimelineLite, TweenMax, Power3 } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function App() {
-  gsap.registerPlugin(ScrollTrigger);
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <Nav />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route exact path="/blog" component={Blog} />
-          <Route path="/blog/:id" component={Post} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route path="/portfolio/:id" component={PortfolioPost} />
-          <Route path="/contact" component={ContactPage} />
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route exact path='/blog' component={Blog} />
+          <Route path='/blog/:id' component={Post} />
+          <Route exact path='/portfolio' component={Portfolio} />
+          <Route path='/portfolio/:id' component={PortfolioPost} />
+          <Route path='/contact' component={ContactPage} />
         </Switch>
       </div>
     </Router>
